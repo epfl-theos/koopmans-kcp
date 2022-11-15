5d8f5634b82fe0f517e4caa628d605542a4bdd3d
Edward Linscott <edwardlinscott@gmail.com>
2022-11-15 15:52:57 +0100

    Add test for github ci

--- tests/run-bin2xml.sh
+++ tests/run-bin2xml.sh
@@ -2,6 +2,7 @@
 
 echo "Running BIN2XML ..."
 echo "${ESPRESSO_ROOT}/bin/bin2xml_real_space_density.x $1 $2 $3"
+ls
 echo "Contents of total density"
 cat 'charge-density.xml'
 echo "Contents of orbital density"
