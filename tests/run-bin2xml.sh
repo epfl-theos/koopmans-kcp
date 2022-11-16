3c6d07fedb2d42d98ddb087420a36746fd2b9e67
Edward Linscott <edwardlinscott@gmail.com>
2022-11-16 10:01:50 +0100

    Fixed wrong path in run-bin2xml

--- tests/run-bin2xml.sh
+++ tests/run-bin2xml.sh
@@ -4,6 +4,6 @@ echo "Running BIN2XML ..."
 echo "${ESPRESSO_ROOT}/bin/bin2xml_real_space_density.x $1 $2 $3"
 ls
 echo "Contents of total density"
-cat $2/'charge-density.xml'
+cat 'charge-density.xml'
 echo "Contents of orbital density"
-cat $2/'orbital.occ.0.00001.xml'
+cat 'orbital.occ.0.00001.xml'
