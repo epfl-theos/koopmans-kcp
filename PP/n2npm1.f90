9fd6e32441c8500d610c43e1dc03307dcdc30bf3
ncolonna <nicola.colonna@psi.ch>
2023-08-18 15:54:44 +0200

    Basic implementation done.

--- PP/n2npm1.f90
+++ PP/n2npm1.f90
@@ -1,4 +1,4 @@
-! TO DO:
+! DONE:
 ! 1) this is the desired comand with arguments from command line
 !      <binary name> --fill  <spin_channel> <index> tmpdir_in tmpdir_out
 !      <binary name> --empty <spin_channel ><index> tmpdir_in tmpdir_out
@@ -7,6 +7,7 @@
 !    and spin. I think it would be convenient to give the spin channel from input as
 !    we have different evc files for different spin ) 
 ! 2) Add reading routine for empty state (only if --fill) 
+! TODO: 
 ! 3) Check Makefile and the need of mpif90 wrapper (it loooks like we need it 
 !    as iotk as some dependendence on mpi library. Also we might need a different 
 !    rule to compile this program (no need for any precompilation flags like e.g. -D__MPI)
@@ -29,14 +30,17 @@ PROGRAM n2npm1
   COMPLEX(DP), ALLOCATABLE :: evc(:,:,:)
   COMPLEX(DP), ALLOCATABLE :: evc_empty(:,:,:)
   COMPLEX(DP), ALLOCATABLE :: evc_out(:,:,:)
-  INTEGER :: ngw, nbnd(2), ispin, nspin, igwx, ibnd, ibnd_, spin_channel
+  INTEGER :: ngw, nbnd(2), ispin, nspin, igwx, ibnd, ibnd_, spin_channel, ig
+  INTEGER :: nbnd_emp(2)
   INTEGER :: nbnd_out(2)
   REAL(DP) :: nel(2)
-  LOGICAL :: is_cmplx, l_fill, gamma_only
+  LOGICAL :: is_cmplx, l_fill, gamma_only, exst
   INTEGER :: index
   REAL(DP) :: scalef
+  INTEGER :: iuni
   !
-  task="empty"            ! FIXME this will be dependent on the input  
+  !Defauls
+  task="fill"             ! FIXME this will be dependent on the input  
   index=1                 ! FIXME this will be dependent on the input  
   dir_in='./'             ! FIXME this will be dependent on the input path
   dir_out='./'            ! FIXME this will be dependent on the input path
@@ -44,8 +48,7 @@ PROGRAM n2npm1
   !                       ! Requires a way determine which spin channel from input (either explicite input or
   !                       ! inferred from index which should go from 1 to nbnd(1)+nbnd(2)
   !
-  !CALL get_command_argument( 1, arg )
-  !CALL parse_args( io_files, nrtot, output_exst )
+  CALL read_command_line (task, index, spin_channel, dir_in, dir_out)
   !
   ! l_fill determine what to do: remove from an occupied one (l_fill=F)
   ! or add to empty (l_fill=T)
@@ -53,13 +56,23 @@ PROGRAM n2npm1
   IF (task=="fill") l_fill=.true.
   !
   IF (l_fill) THEN 
-    WRITE(*, '(/, 3X, 2(A, I5))') "TASK: Going to add &
+    WRITE(*, '(/, 3X, 2(A, I5))') "TASK: N --> N+1; Adding &
             one electron taken from empty orbital= ", index, " spin= ", spin_channel
   ELSE
-    WRITE(*, '(/, 3X, 2(A, I5))') "TASK: Going to remove &
+    WRITE(*, '(/, 3X, 2(A, I5))') "TASK: N --> N-1; Removing &
             one electron from occupied orbital= ", index, " spin= ", spin_channel
   ENDIF
+    WRITE(*,'(/, 5X, "SUMMARY: task     =", A)')  TRIM(task)
+    WRITE(*,'(   5X, "         index    =", I5)') index
+    WRITE(*,'(   5X, "         spin     =", I5)') spin_channel
+    WRITE(*,'(   5X, "         dir_in   =", A)')  TRIM(dir_in)
+    WRITE(*,'(   5X, "         dir_out  =", A)') TRIM(dir_out)
   !
+  IF (dir_in == dir_out) THEN 
+     WRITE(*,'(3X, 3A)') "ERROR: dir_out = dir_in"
+     STOP
+  ENDIF
+  WRITE(*,'(/ 5X, A)') "Reading OCC manifold ..."
   ngw = 0; nbnd=0; nspin=0; nel=0.D0
   ! READ the shape of the wfc object (# of PW, # spin, complx/real, # bands) 
   filename_in=TRIM(dir_in)//'evc01.dat'
@@ -70,7 +83,14 @@ PROGRAM n2npm1
      filename_in=TRIM(dir_in)//'evc02.dat'
      CALL read_sizes (filename_in, ngw, nbnd(2), nspin, is_cmplx, igwx, scalef, gamma_only)
   ENDIF
-  WRITE(*,*) igwx, ngw, nbnd(:), nspin
+  !
+  WRITE(*,'(/, 7X, "SUMMARY: igwx     =", I5)') igwx
+  WRITE(*,'(   7X, "         ngw      =", I5)') ngw
+  WRITE(*,'(   7X, "         nspin    =", I5)') nspin
+  WRITE(*,'(   7X, "         nbnd(1)  =", I5)') nbnd(1)
+  WRITE(*,'(   7X, "         nbnd(2)  =", I5)') nbnd(2)
+  WRITE(*,'(   7X, "         is_cmplx =", L5)') is_cmplx
+  WRITE(*,'(   7X, "         G_trick  =", L5, /)') gamma_only
   !
   ALLOCATE (evc(igwx, max(nbnd(1), nbnd(2)), nspin))
   !
@@ -80,27 +100,78 @@ PROGRAM n2npm1
     IF (ispin==2 ) filename_in=TRIM(dir_in)//'evc02.dat'
     CALL read_wf_occ (filename_in, evc(:,:,ispin), ngw, nbnd(ispin)) 
     CALL check_nele (ngw, nbnd(ispin), evc(:,:,ispin), nel(ispin))
-    WRITE(*,*) ispin, nel(ispin), NINT(nel(ispin)), evc(1,1,ispin), evc(igwx,nbnd(ispin),ispin)! check debug
+    WRITE(*,'(7X, "CHECK: ispin =", I5, " nel =", F18.12, " INT(nel)=", I5)') &
+            ispin, nel(ispin), NINT(nel(ispin))
+    !WRITE(*,) ispin, nel(ispin), NINT(nel(ispin))
   ENDDO
   !
   ! At this stage I read the empty manifold if needed 
   IF (l_fill) THEN
-     ! Here we need to read also empty states if 
-     WRITE(*,*) "Case l_fill = T NOT IMPLEMENTED YET: STOP"
-     STOP
+     WRITE(*,'(/ 5X, A)') "Reading EMP manifold ..."
+     ! Here we need to read also empty states if needed.
+     ! First the shape of the arrey ngw, nbnd_emp for spin up
+     iuni=789
+     filename_in=TRIM(dir_in)//'evc0_empty1.dat'
+     INQUIRE (FILE=TRIM(filename_in), EXIST=EXST)
+     IF (.NOT. exst) THEN 
+        WRITE(*,'(3A)') "File ", TRIM(filename_in), " do not exist"
+        STOP
+     ENDIF
+     OPEN (UNIT=iuni, FILE=TRIM(filename_in), status='unknown', FORM='UNFORMATTED')
+     READ (iuni) ngw, nbnd_emp(1)
+     ! then for spin down channel
+     IF (nspin == 2) THEN 
+       iuni=iuni+1
+       filename_in=TRIM(dir_in)//'evc0_empty2.dat'
+       INQUIRE (FILE=TRIM(filename_in), EXIST=EXST)
+       IF (.NOT. exst) THEN
+          WRITE(*,'(3A)') "File ", TRIM(filename_in), " do not exist"
+          STOP
+       ENDIF
+       OPEN (UNIT=iuni, FILE=TRIM(filename_in), status='unknown', FORM='UNFORMATTED')
+       READ (iuni) ngw, nbnd_emp(2)
+     ENDIF
+     WRITE(*,'(/, 7X, "SUMMARY: ngw      =", I5)') ngw
+     WRITE(*,'(   7X, "         nbnd(1)  =", I5)') nbnd_emp(1)
+     WRITE(*,'(   7X, "         nbnd(2)  =", I5)') nbnd_emp(2)
+     ! allocation
+     ALLOCATE ( evc_empty (ngw, MAX(nbnd_emp(1), nbnd_emp(2)),nspin) )
+     !
+     ! read the empty-state wfcs
+     iuni=789
+     filename_in=TRIM(dir_in)//'evc0_empty1.dat'
+     DO ispin = 1, nspin
+       IF (ispin == 2) filename_in=TRIM(dir_in)//'evc0_empty2.dat'
+       IF (ispin == 2) iuni=iuni+1
+       DO ibnd = 1, nbnd_emp(ispin)
+          READ (iuni) (evc_empty(ig,ibnd,ispin), ig=1, ngw)
+       ENDDO
+     ENDDO
   ENDIF
   !
   ! check if the index of the orbital is within a valid range
   IF (l_fill) THEN
     !
-  ELSE
-    IF (index .gt. nbnd(1)+nbnd(2)) THEN
-       WRITE(*,*) "index out of range ", index, nbnd(1)+nbnd(2)
-       STOP
+    IF (spin_channel == 1 .AND. index .gt. nbnd_emp(1)) THEN
+       WRITE(*,*) "index out of range ", index, nbnd_emp(1);  STOP
     ENDIF
+    IF (spin_channel == 2 .AND. index .gt. nbnd_emp(2)) THEN
+       WRITE(*,*) "index out of range ", index, nbnd_emp(2);  STOP
+    ENDIF
+    !
+  ELSE
+    !
+    IF (spin_channel == 1 .AND. index .gt. nbnd(1)) THEN
+       WRITE(*,*) "index out of range ", index, nbnd(1);  STOP
+    ENDIF
+    IF (spin_channel == 2 .AND. index .gt. nbnd(2)) THEN
+       WRITE(*,*) "index out of range ", index, nbnd(2);  STOP
+    ENDIF
+    !
   ENDIF
   !
   ! From now on we reshape the wfc object
+  WRITE(*,'(/ 5X, A)') "Re-shaping OCC manifold ..."
   nbnd_out = nbnd
   IF (l_fill) THEN 
      nbnd_out(spin_channel) = nbnd(spin_channel) + 1
@@ -128,14 +199,74 @@ PROGRAM n2npm1
      ENDDO
   ENDIF
   !
-  filename_out=TRIM(dir_out)//'evc01_.dat'
+  WRITE(*,'(/, 7X, "SUMMARY: igwx     =", I5)') igwx
+  WRITE(*,'(   7X, "         ngw      =", I5)') ngw
+  WRITE(*,'(   7X, "         nspin    =", I5)') nspin
+  WRITE(*,'(   7X, "         nbnd(1)  =", I5)') MAX(nbnd_out(1),nbnd_out(2))
+  WRITE(*,'(   7X, "         nbnd(2)  =", I5)') MAX(nbnd_out(1),nbnd_out(2))
+  WRITE(*,'(   7X, "         is_cmplx =", L5)') is_cmplx
+  WRITE(*,'(   7X, "         G_trick  =", L5, /)') gamma_only
+  !
+  WRITE(*,'(/ 5X, A)') "Writing new WFCs to disk ..."
+  !
+  filename_out=TRIM(dir_out)//'evc01.dat'
   DO ispin = 1, nspin
-    IF (ispin==2 ) filename_out=TRIM(dir_out)//'evc02_.dat'
+    IF (ispin==2 ) filename_out=TRIM(dir_out)//'evc02.dat'
     CALL write_wf (filename_out, evc_out(:,:,ispin), nspin, ispin, ngw, igwx, &
-            MAX(nbnd(1), nbnd(2)), is_cmplx, scalef, gamma_only)
+            MAX(nbnd_out(1), nbnd_out(2)), is_cmplx, scalef, gamma_only)
   ENDDO
+  !
+  WRITE(*,'(/ 3X, A, /)') "GAME OVER" 
   CONTAINS
   !
+  !----------------------------------------------------------------------
+  SUBROUTINE read_command_line (task, index, spin_channel, dir_in, dir_out) 
+     !------------------------------------------------------------------
+     !
+     IMPLICIT NONE 
+     INTEGER :: index, spin_channel
+     CHARACTER(LEN=256) :: task, dir_in, dir_out
+     INTEGER nargs, iarg, counter
+     CHARACTER(LEN=256) :: arg
+     !
+     nargs = command_argument_count()
+     !WRITE(*,*) nargs
+     !   
+     IF ( nargs /=  5 ) THEN
+        WRITE(*,'(/, 5X, A, I5)') "Wrong number of arguments: STOP", nargs
+        WRITE(*,'(   5X, A, /)') "Usage: n2npm1.x --<task> <index> <spin_channel> <dir_in> <dir_out>"
+        STOP
+     ENDIF
+     !
+     iarg = 1
+     counter = 0
+     !
+     CALL get_command_argument( iarg, arg )
+     iarg = iarg + 1
+     !
+     SELECT CASE ( trim(arg) )
+     CASE ( '--fill' )
+        task="fill"
+     CASE ( '--empty')
+        task="empty"
+     CASE DEFAULT
+       WRITE(*,'(A)') 'unrecognised argument option'
+       STOP
+     END SELECT
+     !
+     CALL get_command_argument( iarg, arg )
+     READ(arg,*) index
+     iarg = iarg + 1
+     CALL get_command_argument( iarg, arg )
+     READ(arg,*) spin_channel
+     iarg = iarg + 1
+     CALL get_command_argument( iarg, dir_in )
+     iarg = iarg + 1
+     CALL get_command_argument( iarg, dir_out )
+     !
+     RETURN
+     !
+  END SUBROUTINE
   !
   !----------------------------------------------------------------------
   SUBROUTINE write_wf(filename, wf, nspin, ispin, ngw, igwx, nbnd, is_cmplx, scalef, gamma_only)
@@ -245,7 +376,7 @@ PROGRAM n2npm1
     CALL iotk_scan_attr( attr, "gamma_only",  gamma_only )
     CALL iotk_scan_attr( attr, "scale_factor", scalef )
     !
-    WRITE(*,*) "NICOLA", ispin, ngw, nbnd, ik, nk, nspin, igwx, is_cmplx, scalef
+    !WRITE(*,*) "NICOLA", ispin, ngw, nbnd, ik, nk, nspin, igwx, is_cmplx, scalef
     CALL iotk_close_read( iuni )
     RETURN
     !
@@ -284,14 +415,14 @@ PROGRAM n2npm1
     CALL iotk_scan_empty( iuni, "INFO", attr )
     !
     ngw_=0; nbnd_=0; 
-    WRITE(*,*) "NICOLA", ngw_, nbnd_
+    !WRITE(*,*) "NICOLA", ngw_, nbnd_
     CALL iotk_scan_attr( attr, "ngw",          ngw_ )
     CALL iotk_scan_attr( attr, "nbnd",         nbnd_ )
     CALL iotk_scan_attr( attr, "igwx",         igwx )
     CALL iotk_scan_attr( attr, "ispin",        ispin )
     CALL iotk_scan_attr( attr, "do_wf_cmplx",  is_cmplx )
     !
-    WRITE(*,*) "NICOLA", TRIM(filename), ngw_, nbnd_, ispin, igwx, is_cmplx
+    !WRITE(*,*) "NICOLA", TRIM(filename), ngw_, nbnd_, ispin, igwx, is_cmplx
     !
     IF (nbnd_ /= nbnd .OR. ngw_ /= ngw) THEN 
        WRITE(*,*) "Size Mismatch. STOP"
